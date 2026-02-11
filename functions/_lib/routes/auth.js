c73be5c721a34d04b95c155adb2db369785c0590
0xbiao <batoniyan@gmail.com>
2026-02-12 01:31:34 +0800

    1

--- functions/_lib/routes/auth.js
+++
@@ -1,6 +0,0 @@
-export function registerAuthRoutes(app) {
-    app.post('/login', async (c) => {
-        const { password } = await c.req.json();
-        return c.json({ success: password === c.env.ADMIN_PASSWORD })
-    })
-}
